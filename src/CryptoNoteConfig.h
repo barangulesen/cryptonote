73673026ea9506c6d3cb1999ae43e8d85438bc84
Baran Gulesen <barangulesen@gmail.com>
2020-04-08 12:11:25 +0300

    change dnsseed
    amazon seed us

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "52.15.205.33:10453",
+  "18.216.155.123:10453",
   "13.59.145.207:10453",
 };
 
